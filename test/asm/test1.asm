149b4795d56b418363bcb732e258e61e2c7d9c8d
Cam Brown <cambrown@u.northwestern.edu>
2023-06-05 00:43:00 -0500

    added test file mods

--- test/asm/test1.asm
+++ test/asm/test1.asm
@@ -1,21 +1,26 @@
-addi x1, x0, 24
-addi x2, x0, 32
+addi x1, x0, 1
+addi x2, x0, 2
 nop
 nop
 
 add  x3, x1, x2
-addi x4, x0, 1
-nop
-nop
-
-slli x4, x4, 28
-nop
+addi x4, x0, 100
 nop
 nop
 
 nop
 nop
-lw  x5, x4, x0
+nop
+nop
+
+nop
+nop
+lw x4, 0(x4)
+nop
+
+nop
+nop
+nop
 nop
 
 add x5, x5, x4
